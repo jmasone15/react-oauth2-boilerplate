5cdc43d7e40f313e24c2a8162e5c3eea07491c8d
Jordan Masone <jordanmasone1@gmail.com>
2022-01-29 10:46:55 -0500

    Added user email to home page.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../context/AuthContext';
 
 export default function Home() {
+
+    const { userEmail } = useContext(AuthContext);
+
     return (
         <div className="App">
             <h1>Home Page</h1>
+            <h3>Welcome {userEmail}!</h3>
         </div>
-      );
+    );
 }
